69e68ac83b466cdbc2b52c1798240e991a030372
“selam” <Selamkd@users.noreply.github.com>
2024-07-31 12:15:52 +0100

    add test classes

---
+++ PetStore_API_Testing/src/test/java/com/sparta/test_petstore/AppConfig.java
@@ -0,0 +1,44 @@
+package com.sparta.test_petstore;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AppConfig {
+
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream inputStream = AppConfig.class
+                .getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            if (inputStream != null) {
+                properties.load(inputStream);
+            } else {
+                throw new IOException("Unable to find config.properties");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getBaseUri() {
+        return properties.getProperty("api_url");
+    }
+
+    public static String getToken() {
+        return properties.getProperty("api_token");
+    }
+
+    public static String getRepoPath() {
+        return properties.getProperty("repo_path");
+    }
+
+    public  static String getOwner(){
+        return properties.getProperty("owner");
+    }
+
+    public  static String getRepoName(){
+        return properties.getProperty("repo_name");
+    }
+}
---
+++ PetStore_API_Testing/src/test/java/com/sparta/test_petstore/petapi_tests/GetPetByIdTests.java
@@ -0,0 +1,4 @@
+package com.sparta.test_petstore.petapi_tests;
+
+public class GetPetByIdTests {
+}
